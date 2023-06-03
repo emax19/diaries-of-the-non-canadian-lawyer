19a2da4e993b099da33d4b69faee8efc4c44b713
Eugene_Maksymets <jenia2009m2@gmail.com>
2023-06-02 17:02:30 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Hi, I'm Valeriia`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Valeriia Ustiuhova`,
+      summary: `who lives and works in Canada building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
